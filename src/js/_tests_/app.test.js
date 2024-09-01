1d893aa3eb1d8ee77f73174396ff8dcd27a41773
SergeyBildanov <troktrokov@gmail.com>
2024-09-01 20:28:19 +0400

    test change

--- src/js/_tests_/app.test.js
+++ src/js/_tests_/app.test.js
@@ -13,24 +13,22 @@ beforeAll(() => {
 
 
 jest.setTimeout(30000);
-describe('Popover work', () => {
-    test("Popover adding", ()=>{
+describe('Popover work',  () => {
+    test("Popover adding", async ()=>{
         const button = dom.window.document.querySelector('.button');
         const popoverFactory = new Popover();
         button.dispatchEvent(new dom.window.Event('click'));
         setTimeout(() => {
             expect(popoverFactory._popovers.length).toBe(1);
-            done();
         }, 1001);
     })
-    test("Popover removing", ()=>{
+    test("Popover removing", async ()=>{
         const button = dom.window.document.querySelector('.button');
         const popoverFactory = new Popover();
         button.dispatchEvent(new dom.window.Event('click'));
         button.dispatchEvent(new dom.window.Event('click'));
         setTimeout(() => {
             expect(popoverFactory._popovers.length).toBe(0);
-            done();
         }, 1001);
     })
 });
